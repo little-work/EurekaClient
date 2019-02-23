710d070ae640cf2a65e1e7b364cbbafc89d79c75
lilin <1142114078@qq.com>
2019-02-23 22:22:57 +0800

    添加了验证feign

--- src/main/java/com/sunyard/eurekaclient/controller/HelloSpringBoot.java
+++ src/main/java/com/sunyard/eurekaclient/controller/HelloSpringBoot.java
@@ -1,11 +1,11 @@
 package com.sunyard.eurekaclient.controller;
 
+import com.sunyard.eurekaclient.bean.User;
 import com.sunyard.eurekaclient.utils.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloSpringBoot {
@@ -23,9 +23,33 @@ public class HelloSpringBoot {
         return helloService.who();
     }
 
+    /**
+     * 不带参数一个接口
+     * @return
+     */
     @RequestMapping(value = "hello")
-    public String helloCloud(){
-        return "helloword";
+    public String helloservice(){
+        return "分布式服务中一个不带参数的接口验证";
+    }
+
+
+    /**
+     * get请求参数
+     * @return
+     */
+    @RequestMapping(value = "hello2",method = RequestMethod.GET)
+    public String HelloService2(@RequestParam String name){
+        return "GET请求方式，传进来的参数是："+name;
+    }
+
+    /**
+     * post的请求方式
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "hello3",method = RequestMethod.POST)
+    public String helloservice3(@RequestBody User user){
+        return "POST请求方式，传进来的参数是："+user.getName()+user.getAge();
     }
 
     @RequestMapping(value = "info")
